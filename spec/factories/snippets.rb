226bbc868cb5291e695fac8c96c405042481d17e
hawk73 <hawk.bms@gmail.com>
2017-03-01 01:22:37 +0400

    Annotate.

--- spec/factories/snippets.rb
+++ spec/factories/snippets.rb
@@ -6,3 +6,20 @@ FactoryGirl.define do
     user
   end
 end
+
+# == Schema Information
+#
+# Table name: snippets
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  name       :string           not null
+#  text       :text
+#  settings   :jsonb
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_snippets_on_user_id  (user_id)
+#
